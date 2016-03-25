73700ef786e7cce3e8f0ede3a27a944b5e3615c3
RaRaMe <r.ram2010@live.com>
2016-03-25 16:29:54 -0400

    testcommit

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/Setup.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/Setup.java
@@ -45,7 +45,7 @@ public class Setup extends AppCompatActivity {
                 "Weight cannot be empty!"
         };
 
-        //craptest3
+        //craptest4
 
         if(isEmpty(editTextArray,errorMessages)) return;
 
