bfd424cd7ece21d6b59d1a136d12dd8faadecff3
rachelauren123 <rgremillion2012@gmail.com>
2016-03-31 02:33:41 -0400

    Test

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/UserProfile.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/UserProfile.java
@@ -9,7 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+//Test Push
 public class UserProfile extends AppCompatActivity {
 
     TextView fullNameTV, ageTV, genderTV, heightTV, weightTV, bmiTV, sedentaryTV, lightlyActiveTV, moderatelyActiveTV, veryActiveTV, extremelyActiveTV, myLogsTV;
