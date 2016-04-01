13d58d5bbcfb3e5be99aa21380f7da9c93fc3ce0
Brandon Johnson <bjohnson@sunlightsolutions.com>
2016-04-01 00:25:01 -0400

    Create workout changes

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/CreateWorkout.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/CreateWorkout.java
@@ -10,9 +10,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -40,20 +42,23 @@ public class CreateWorkout extends AppCompatActivity {
 
         exerciseLV = (ListView) findViewById(R.id.exercisesLV);
 
+        //populate list of exercises
         populateExercises();
 
 
     }
 
-
+    //Method to populate list of exercises
     protected void populateExercises(){
-
-
+        //create array to store exercise names
         final ArrayList<String> exercisesList = new ArrayList<>();
 
+        //create the database manager
         DataBaseHelper myDbHelper;
         myDbHelper = new DataBaseHelper(this);
 
+        //create database if it does not exist (or was erased)
+        //open if it is already there
         try {
             myDbHelper.createDataBase();
         } catch (IOException ioe) {
@@ -66,31 +71,67 @@ public class CreateWorkout extends AppCompatActivity {
             throw sqle;
         }
         try {
+            //get actual database from manager (the one we just opened)
             SQLiteDatabase exerciseDB = myDbHelper.getReadableDatabase();
 
+            //set cursor to start traversing search
+            //raw query is SQL code (Select everything from table "exercises" and order them by name)
             Cursor c = exerciseDB.rawQuery("SELECT * FROM exercises ORDER BY name", null);
 
+            //get items from "name" column of table
             int nameIndex = c.getColumnIndex("name");
 
+            //move cursor to to of list (table)
             c.moveToFirst();
 
+            //add exercise to list and move cursor to next exercise
             while (c != null) {
 
                exercisesList.add(c.getString(nameIndex));
                 c.moveToNext();
             }
 
+            //close database to avoid memory leak
             exerciseDB.close();
         }catch (Exception e){
             e.printStackTrace();
         }
 
+        //adapter to get list into ListView
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, exercisesList);
         exerciseLV.setAdapter(adapter);
+        //check multiple exercises at a time
         exerciseLV.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+    }
 
 
+    //************Still Working On  @@@Brandon
+    //Code to save a new workout (will add to database)
+    public void saveCustomWorkout(View view)
+    {
+        EditText workoutNameED = (EditText) findViewById(R.id.createWorkoutName);
+        String workoutNameStr = workoutNameED.getText().toString();
+
+        SparseBooleanArray checked = exerciseLV.getCheckedItemPositions();
+
+        if (workoutNameStr == ""){
+            Toast.makeText(getApplicationContext(),"The workout must have a name.",Toast.LENGTH_SHORT).show();
+        }
+        else if(checked.size()==0){
+            Toast.makeText(getApplicationContext(),"You must select at least one exercise.",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String exercisesForWorkoutStr = "";
+
+            for (int i = 0; i < exerciseLV.getAdapter().getCount(); i++) {
+                if (checked.get(i)) {
+                    exercisesForWorkoutStr += exerciseLV.getItemAtPosition(i).toString() + "|";
+                }
+            }
+            exercisesForWorkoutStr = exercisesForWorkoutStr.substring(0, exercisesForWorkoutStr.length() - 1);
+            Log.i("Test", exercisesForWorkoutStr);
+        }
 
     }
 
