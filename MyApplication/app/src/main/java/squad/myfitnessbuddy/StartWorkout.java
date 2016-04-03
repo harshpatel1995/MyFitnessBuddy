a7dcb2389d4ab4ce44c5a6682193871cf7bd0ca1
Brandon Johnson <bjohnson@sunlightsolutions.com>
2016-04-03 17:25:38 -0400

    file name and gloating button change

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/StartWorkout.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/StartWorkout.java
@@ -18,7 +18,7 @@ public class StartWorkout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_workout);
+        setContentView(R.layout.start_workout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,16 +28,6 @@ public class StartWorkout extends AppCompatActivity {
         final ArrayList <String> myExerciseList = new ArrayList<>();
 
 
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
