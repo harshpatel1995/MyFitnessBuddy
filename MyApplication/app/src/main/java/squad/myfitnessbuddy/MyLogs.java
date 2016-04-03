3f8169f24e143c017e2abd3dbc9d0f7c9d07b782
harshpatel1995 <harshpatel@knights.ucf.edu>
2016-04-03 17:23:38 -0400

    Added function to populate logs

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/MyLogs.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/MyLogs.java
@@ -1,6 +1,8 @@
 package squad.myfitnessbuddy;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
 
 
 public class MyLogs extends MenuButtonBar {
+
+    SQLiteDatabase exerciseDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,44 @@ public class MyLogs extends MenuButtonBar {
         assert actionBar != null;
         actionBar.setTitle("My Logs");
 
+        try {
+            exerciseDB = this.openOrCreateDatabase("mfbDatabase.db", MODE_PRIVATE, null);
+            exerciseDB.execSQL(ConstantValues.cCREATE_OR_OPEN_WORKOUT_LOGS_DATABASE_SQL);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        populateLogs();
+    }
+
+    protected void populateLogs() {
+
+        int id;
+        String date;
+        String exerciseName;
+        int reps;
+        int weight;
+
+        Cursor c = exerciseDB.rawQuery("SELECT * FROM logs", null);
+        int idColumn = c.getColumnIndex("_id");
+        int dateColumn = c.getColumnIndex("date");
+        int exerciseColumn = c.getColumnIndex("exercise");
+        int repsColumn = c.getColumnIndex("reps");
+        int weightColumn = c.getColumnIndex("weight");
+
+        c.moveToLast();
+
+        while(c != null) {
+            id = c.getInt(idColumn);
+            date = c.getString(dateColumn);
+            exerciseName = c.getString(exerciseColumn);
+            reps = c.getInt(repsColumn);
+            weight = c.getInt(weightColumn);
+            System.out.println(id + " " + date + " " + exerciseName + " " + reps + " " + weight +" ");
+            c.moveToPrevious();
+        }
+
+        c.close();
     }
 }
