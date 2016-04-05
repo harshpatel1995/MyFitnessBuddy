674a91392ad422f6dcdf07524b5fb1cd2f5bf64f
RaRaMe <r.ram2010@live.com>
2016-04-05 03:49:01 -0400

    Change Setup Page
    Layout fix
    input restrictions
    reordered fields
    alignment fix
    next field on enter

--- MyApplication/app/src/main/java/squad/myfitnessbuddy/Setup.java
+++ MyApplication/app/src/main/java/squad/myfitnessbuddy/Setup.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
